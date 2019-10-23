ae9994593bd4e5e3d5ad1c29eed7890cc0a1baf3
Daniel Johansen <dajoh16@student.sdu.dk>
2019-10-23 13:56:57 +0200

    Own implementation of Lcg added

--- RandomGeneratorTest/Program.cs
+++ RandomGeneratorTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RandomGeneratorTest
 {
@@ -6,7 +8,46 @@ namespace RandomGeneratorTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rand1 = Random(123456789,100,101427,321,65536);
+            var rand2 = Random(123456789,100,65539,0,2147483648);
+            var rand3 = CollectCSharpRandom(123456789,100);
+            bool rejected = KolmogorowSmirnow(rand1, 0.05);
+        }
+
+        private static bool KolmogorowSmirnow(List<double> rand1, double alpha)
+        {
+            return false;
+        }
+
+
+        static List<double> Random(long seed, int size, long a, long c, long m)
+        {
+            List<double> toReturn = new List<double>();
+            long xi = seed;
+            foreach (var i in Enumerable.Range(0, size))
+            {
+                xi = lcg(xi,a,c,m);
+                double rand = ((double) xi) / ((double) (m));
+                toReturn.Add(rand);
+            }
+            return toReturn;
+        }
+        static long lcg(long xi, long a, long c, long m)
+        {
+            return ((a * xi + c) % m);
+        }
+        
+        private static List<double> CollectCSharpRandom(int seed, int size)
+        {
+            List<double> toReturn = new List<double>();
+            Random random = new Random(seed);
+            foreach (var i in Enumerable.Range(0, size))
+            {
+                var rand = random.NextDouble();
+                toReturn.Add(rand);
+            }
+            return toReturn;
         }
     }
+    
 }
